30f4e4d91bff132da5f6a8ceef8cf5173d9f7524
alexey-savin <alexeyv.savin@gmail.com>
2017-06-16 11:49:54 +0300

    DictService

---
+++ ThreadSafeDictionary/ThreadSafeDictionary/DictionaryService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadSafeDictionary
+{
+    public class DictionaryService
+    {
+        private Dictionary<string, Guid> _keys = new Dictionary<string, Guid>();
+
+        public Guid GetKeyByName(string keyName)
+        {
+            if (_keys.ContainsKey(keyName))
+            {
+                return _keys[keyName];
+            }
+
+            Guid key = Guid.NewGuid();
+            _keys.Add(keyName, key);
+
+            return key;
+        }
+    }
+}
